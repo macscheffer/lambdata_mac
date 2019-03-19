0bf935e2bdbc8fc1f885283905de4ee8b8ee46c9
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-03-19 13:50:09 -0400

    Update lambdata_mac.py

--- lambdata_mac.py
+++ lambdata_mac.py
@@ -1,3 +1,16 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import random
+
+
+
 def date_to_features(col, df=df, drop=False):
     
     # a function to split a pandas datetime column into a different date columns (year, month, day, day of week, day of year)
@@ -40,3 +53,29 @@ def square_features(df, features):
     
     return df
     
+
+
+def macs_pca_features(df, cols, pcs, clusters, prefix):
+     
+     #add a function/functionality to 
+     #vizualize sufficient pcs
+
+    
+     # takes in a df. columns to reduce   dimensions, how many dimensions to reduce to, and how many clusters to make.
+     
+     
+    df = df.copy()
+
+    scaler = StandardScaler()
+    std_df = scaler.fit_transform(df[cols])
+     
+    pca = PCA(pcs)
+    pc_df = pca.fit_transform(std_df)
+     
+
+    for cluster in clusters:
+        km = KMeans(cluster)
+        km = km.fit(pc_df)
+        df[prefix + str(pcs) + 'PCs_' + str(cluster) + 'Means_Cluster'] = km.labels_
+
+    return df
