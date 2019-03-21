bb9bfeea87b4594d755746bcc3c9bf81abf378d0
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-03-21 11:29:00 -0400

    Update lambdata_mac.py

--- lambdata_mac.py
+++ lambdata_mac.py
@@ -28,21 +28,6 @@ def date_to_features(col, df=df, drop=False):
      return df
      
      
-def macs_labelEncode_meaningful(df, train, cols, target):
-    
-     # takes in a list of columns to encode in a linear fashion. 
-     
-     for col in cols:
-        
-          cur_encodes = pd.factorize(train.pivot_table(index=col, values=target).sort_values(target).index)[1].tolist()
-          fnl_encodes = pd.factorize(train.pivot_table(index=col, values=target).sort_values(target).index)[0].tolist()
-        
-          mapper_dic = dict(zip(cur_encodes, fnl_encodes))
-        
-          df['MeaningfulEnc_' + col] = df[col].map(mapper_dic)
-        
-     return df
-     
      
 def square_features(df, features):
     
@@ -79,3 +64,59 @@ def macs_pca_features(df, cols, pcs, clusters, prefix):
         df[prefix + str(pcs) + 'PCs_' + str(cluster) + 'Means_Cluster'] = km.labels_
 
     return df
+
+
+
+
+def mac_labelEncode(df, min_instances, cols):
+    
+    # encodes columns as long as it means some minumum number of instances
+    
+    for col in cols:
+        
+        new_labels = {}
+        for k, v in zip(df[col].value_counts().index.tolist(), df[col].value_counts().values.tolist()):
+            
+            if v > min_instances:
+                new_labels[k] = k
+            else:
+                new_labels[k] = 'misc'
+        
+        df['Encoded_' + col] = pd.factorize(df[col].map(new_labels))[0]
+        
+    return df
+
+
+
+
+def mac_labelEncode_linear(df, train, cols, target):
+    
+     # takes in a list of columns to encode in a linear fashion.
+     
+    for col in cols:
+        
+        cur_encodes = pd.factorize(train.pivot_table(index=col, values=target).sort_values(target).index)[1].tolist()
+        fnl_encodes = pd.factorize(train.pivot_table(index=col, values=target).sort_values(target).index)[0].tolist()
+        
+        mapper_dic = dict(zip(cur_encodes, fnl_encodes))
+        
+        df['MeaningfulEnc_' + col] = df[col].map(mapper_dic)
+    
+    return df
+
+
+
+def mac_create_dummies_meaningful(df, train, cols, target, target_mean_value, cutoff):
+    
+    # creates dummies if the category meets some information standard.
+    
+    for col in cols:
+        
+        for k, v in zip((abs(train.pivot_table(index=col, values=target) - target_mean_value) > cutoff).index.tolist(),
+                        (abs(train.pivot_table(index=col, values=target) - target_mean_value) > cutoff)[target].values):
+            if v:
+                train['dummy_' + col + str(k)] = (train[col] == k)
+                df['dummy_' + col + str(k)] = (df[col] == k)
+        
+    return train, df
+
